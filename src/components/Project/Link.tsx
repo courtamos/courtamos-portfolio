372982c85d6dcbb854ab5e5cf390b7dbc43d0bf0
Courtney Amos <c.amos@shaw.ca>
2021-06-24 12:16:32 -0700

    create link component for projects

---
+++ src/components/Project/Link.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+interface Props {
+  deployedLink: string;
+  githubLink: string;
+}
+
+export const Link: React.FC<Props> = ({ deployedLink, githubLink }) => {
+  return (
+    <>
+      <div className="deployed-link">
+        <a href={deployedLink} target="_blank" rel="noreferrer">
+          <p>
+            Visit the site <FontAwesomeIcon icon={faLink} size="1x" />
+          </p>
+        </a>
+      </div>
+      <div className="github-link">
+        <a href={githubLink} target="_blank" rel="noreferrer">
+          <p>
+            See the code <FontAwesomeIcon icon={faGithub} size="1x" />
+          </p>
+        </a>
+      </div>
+    </>
+  );
+};
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "./Link";
 import { Technology } from "./Technology";
 
 export const Project = () => {
@@ -6,12 +7,18 @@ export const Project = () => {
     <section>
       <h1>Projects</h1>
       <h3>Technology Used</h3>
-      <div className="tech-stack">
+      <div className="tech-stack" style={{ marginBottom: "20px" }}>
         <Technology techUsed="JavaScript" />
         <Technology techUsed="TypeScript" />
         <Technology techUsed="React" />
       </div>
       <h3>See it Yourself</h3>
+      <div className="links">
+        <Link
+          githubLink="https://github.com/courtamos"
+          deployedLink="https://github.com/courtamos"
+        />
+      </div>
     </section>
   );
 };
