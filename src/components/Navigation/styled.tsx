d1ba1f0722b11497422248b9cf48222d4b46c257
Courtney Amos <c.amos@shaw.ca>
2021-06-26 14:33:57 -0700

    style changes to scroll to functionality

--- src/components/Navigation/styled.tsx
+++ src/components/Navigation/styled.tsx
@@ -7,17 +7,21 @@ export const StyledNavbar = styled(Navbar)`
 
 export const StyledNavbarBrand = styled(NavbarBrand)`
   color: black;
-  &:hover {
+  &:hover,
+  .active {
     color: pink;
     border-bottom: 2px solid pink;
+    text-decoration: none;
   }
 `;
 
 export const StyledNavLink = styled(NavLink)`
   color: black;
   padding-bottom: 3px;
-  &:hover {
+  &:hover,
+  .active {
     color: pink;
     border-bottom: 2px solid pink;
+    text-decoration: none;
   }
 `;
